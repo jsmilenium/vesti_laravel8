1e9f01b9c80c8e6e3053a18c7355c5937bfa2619
= <jsmilenium2003@hotmail.com>
2021-04-07 22:03:44 -0300

    Produto Test

--- tests/Feature/ProdutoControllerTest.php
+++ tests/Feature/ProdutoControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Produto;
 
 class ProdutoControllerTest extends TestCase
 {
@@ -13,10 +14,55 @@ class ProdutoControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testIndex()
     {
-        $response = $this->get('/produtos');
+        $response = $this->getJson('/api/produtos');
 
         $response->assertStatus(200);
     }
+
+    public function testShow()
+    {
+        $response = $this->getJson('/api/produtos');
+
+        $response->assertStatus(200);
+    }
+    
+    public function testUpdate()
+    {
+        $random = rand(2,50);
+
+        $response = $this->postJson('/api/produtos/1', [
+                        'codigo' => "'$random'",
+                        'nome' => "'teste$random'",
+                        'composicao' => 'teste',
+                        'tamanho' => 'teste',
+                        'quantidade' => 'teste'
+                        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testStore()
+    {
+        $random = rand(2,50);
+
+        $response = $this->postJson('/api/produtos', [
+                        'codigo' => "'$random'",
+                        'nome' => "'teste$random'",
+                        'composicao' => 'teste',
+                        'tamanho' => 'teste',
+                        'quantidade' => 'teste'
+                        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->deleteJson('/api/produtos/1');
+
+        $response->assertStatus(200);
+    }
+
 }
